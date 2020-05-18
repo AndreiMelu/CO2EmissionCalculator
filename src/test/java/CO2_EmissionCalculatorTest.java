be4ba07567cc27698cc27e20492075ed492d6e76
Andrei <melu.andrei@yahoo.com>
2020-05-18 20:47:15 +0300

    Adding a few more tests

--- src/test/java/CO2_EmissionCalculatorTest.java
+++ src/test/java/CO2_EmissionCalculatorTest.java
@@ -4,8 +4,10 @@ import static Util.CO2_CalculatorUtil.calculateCO2;
 import static Util.CO2_CalculatorUtil.calculateCO2PerCategory;
 import static Util.Category.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CO2_EmissionCalculatorTest {
+
     @Test
     public void defaultValuesTest() {
         //Small_cars
@@ -57,6 +59,38 @@ public class CO2_EmissionCalculatorTest {
                 calculateCO2(1, "km", 1, "medium-electric-car");
 
         assertEquals(defaultSmallCars, calculateCO2PerCategory(1, "km", 1, Small_car));
-        assertEquals(defaultSmallCars, calculateCO2PerCategory(1, "km", 1, Medium_car));
+        assertEquals(defaultMediumCars, calculateCO2PerCategory(1, "km", 1, Medium_car));
+    }
+
+    @Test
+    public void testDistanceDefaultValues() {
+        //Small_cars
+        assertEquals(142, calculateCO2(1000, "m", 1, "small-diesel-car"));
+        assertEquals(154, calculateCO2(1000, "m", 1, "small-petrol-car"));
+        assertEquals(73, calculateCO2(1000, "m", 1, "small-plugin-hybrid-car"));
+        assertEquals(50, calculateCO2(1000, "m", 1, "small-electric-car"));
+
+        //Medium_cars
+        assertEquals(171, calculateCO2(1000, "m", 1, "medium-diesel-car"));
+        assertEquals(192, calculateCO2(1000, "m", 1, "medium-petrol-car"));
+        assertEquals(110, calculateCO2(1000, "m", 1, "medium-plugin-hybrid-car"));
+        assertEquals(58, calculateCO2(1000, "m", 1, "medium-electric-car"));
+
+        //Large_cars
+        assertEquals(209, calculateCO2(1000, "m", 1, "large-diesel-car"));
+        assertEquals(282, calculateCO2(1000, "m", 1, "large-petrol-car"));
+        assertEquals(126, calculateCO2(1000, "m", 1, "large-plugin-hybrid-car"));
+        assertEquals(73, calculateCO2(1000, "m", 1, "large-electric-car"));
+
+
+        //Train
+        assertEquals(6, calculateCO2(1000, "m", 1, "train"));
+        assertEquals(calculateCO2(1, "km", 1, "train"),
+                calculateCO2(1000, "m", 1, "train"));
+
+        //Bus
+        assertEquals(27, calculateCO2(1000, "m", 1, "bus"));
+        assertTrue(calculateCO2(1, "km", 1, "bus") ==
+                calculateCO2(1000, "m", 1, "bus"));
     }
 }
